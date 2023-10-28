768b24e1c1bffef53efbfc620d9ccb963143c73e
anapatino <asofiapatino@unicesar.edu.co>
2023-10-28 17:34:11 -0500

    module test completed

--- lib/ui/pages/pageTest/test.dart
+++ lib/ui/pages/pageTest/test.dart
@@ -2,7 +2,6 @@ import 'package:finanlearn/ui/utils/dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../../widgets/question_card.dart';
 import '../dashboard/dashboard.dart';
 
 class Test extends StatefulWidget {
@@ -13,6 +12,32 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
+  String selectedOption1 = '';
+  String selectedOption2 = '';
+  String selectedOption3 = '';
+  String selectedOption4 = '';
+
+  List<String>? listOptions1 = [
+    '300 euros',
+    '340 euros',
+    '100 euros',
+  ];
+  List<String>? listOptions2 = [
+    '400 euros',
+    '340 euros',
+    '100 euros',
+  ];
+  List<String>? listOptions3 = [
+    '1060 euros',
+    '1200 euros',
+    '100 euros',
+  ];
+  List<String>? listOptions4 = [
+    '9611.57 ',
+    '961.57',
+    '9611.27',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,86 +82,347 @@ class _TestState extends State<Test> {
                     horizontal: Dimensions.screenWidth * 0.03),
                 child: SizedBox(
                   height: Dimensions.screenHeight * 0.70,
-                  child: ListView(
-                    children: const [
-                      QuestionCard(
-                        title: 'Pregunta 1',
-                        description:
-                            '¿Cuál es el interés generado por un capital de 5000 euros al 3% anual durante 2 años?',
-                        listOptions: {
-                          'opcion1': '300 euros',
-                          'opcion2': '340 euros',
-                          'opcion3': '100 euros',
-                        },
-                        result: '300 euros',
-                        color: Color.fromRGBO(100, 220, 185, 1),
+                  child: ListView(children: [
+                    Container(
+                      width: Dimensions.screenWidth * 0.90,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
                       ),
-                      QuestionCard(
-                        title: 'Pregunta 2',
-                        description:
-                            '¿Cuál es el interés generado por un capital de 10000 euros al 4% semestral durante 1 año?',
-                        listOptions: {
-                          'opcion1': '400 euros',
-                          'opcion2': '340 euros',
-                          'opcion3': '100 euros',
-                        },
-                        result: '400 euros',
-                        color: Color.fromRGBO(11, 138, 47, 1),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Pregunta 1',
+                              style: GoogleFonts.inter(
+                                color: const Color.fromRGBO(100, 220, 185, 1),
+                                fontSize: Dimensions.screenWidth * 0.06,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          SizedBox(
+                            height: Dimensions.screenHeight * 0.01,
+                          ),
+                          Text(
+                              '¿Cuál es el interés generado por un capital de 5000 euros al 3% anual durante 2 años?',
+                              style: GoogleFonts.inter(
+                                color: Colors.black,
+                                fontSize: Dimensions.screenWidth * 0.045,
+                                fontWeight: FontWeight.w400,
+                              )),
+                          SizedBox(
+                            width: Dimensions.screenHeight * 0.01,
+                          ),
+                          Column(
+                            children: listOptions1!.map((String key) {
+                              return Row(
+                                children: [
+                                  Radio(
+                                    value: key,
+                                    groupValue: selectedOption1,
+                                    onChanged: (val) {
+                                      setState(() {
+                                        selectedOption1 = key;
+                                      });
+                                    },
+                                    fillColor: MaterialStateProperty
+                                        .resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                        return Colors.black;
+                                      },
+                                    ),
+                                  ),
+                                  Text(
+                                    key,
+                                    style: GoogleFonts.inter(
+                                      color: Colors.black,
+                                      fontSize: Dimensions.screenWidth * 0.045,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }).toList(),
+                          )
+                        ],
                       ),
-                      QuestionCard(
-                        title: 'Pregunta 3',
-                        description:
-                            'Carlos toma prestados 800 a una tasa de interés simple del 6.5% durante 5 años. ¿Cuál será el monto total a pagar al final?',
-                        listOptions: {
-                          'opcion1': '1060',
-                          'opcion2': '1200',
-                          'opcion3': '100',
-                        },
-                        result: '1060',
-                        color: Color.fromRGBO(100, 220, 185, 1),
+                    ),
+                    SizedBox(
+                      height: Dimensions.screenHeight * 0.01,
+                    ),
+                    Container(
+                      width: Dimensions.screenWidth * 0.90,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
                       ),
-                      QuestionCard(
-                        title: 'Pregunta 4',
-                        description:
-                            '¿Cuál es el capital que se debe invertir para obtener un monto final de 10000 euros al 2% trimestral durante 2 años?',
-                        listOptions: {
-                          'opcion1': '9611.57',
-                          'opcion2': '961.57',
-                          'opcion3': '9611.27',
-                        },
-                        result: '9611.57',
-                        color: Color.fromRGBO(11, 138, 47, 1),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Pregunta 2',
+                              style: GoogleFonts.inter(
+                                color: const Color.fromRGBO(100, 220, 185, 1),
+                                fontSize: Dimensions.screenWidth * 0.06,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          SizedBox(
+                            height: Dimensions.screenHeight * 0.01,
+                          ),
+                          Text(
+                              '¿Cuál es el interés generado por un capital de 10000 euros al 4% semestral durante 1 año?',
+                              style: GoogleFonts.inter(
+                                color: Colors.black,
+                                fontSize: Dimensions.screenWidth * 0.045,
+                                fontWeight: FontWeight.w400,
+                              )),
+                          SizedBox(
+                            width: Dimensions.screenHeight * 0.01,
+                          ),
+                          Column(
+                            children: listOptions2!.map((String key) {
+                              return Row(
+                                children: [
+                                  Radio(
+                                    value: key,
+                                    groupValue: selectedOption2,
+                                    onChanged: (val) {
+                                      setState(() {
+                                        selectedOption2 = key;
+                                      });
+                                    },
+                                    fillColor: MaterialStateProperty
+                                        .resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                        return Colors.black;
+                                      },
+                                    ),
+                                  ),
+                                  Text(
+                                    key,
+                                    style: GoogleFonts.inter(
+                                      color: Colors.black,
+                                      fontSize: Dimensions.screenWidth * 0.045,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }).toList(),
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: Dimensions.screenHeight * 0.01,
+                    ),
+                    Container(
+                      width: Dimensions.screenWidth * 0.90,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Pregunta 3',
+                              style: GoogleFonts.inter(
+                                color: const Color.fromRGBO(100, 220, 185, 1),
+                                fontSize: Dimensions.screenWidth * 0.06,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          SizedBox(
+                            height: Dimensions.screenHeight * 0.01,
+                          ),
+                          Text(
+                              'Carlos toma prestados 800 a una tasa de interés simple del 6.5% durante 5 años. ¿Cuál será el monto total a pagar al final?',
+                              style: GoogleFonts.inter(
+                                color: Colors.black,
+                                fontSize: Dimensions.screenWidth * 0.045,
+                                fontWeight: FontWeight.w400,
+                              )),
+                          SizedBox(
+                            width: Dimensions.screenHeight * 0.01,
+                          ),
+                          Column(
+                            children: listOptions3!.map((String key) {
+                              return Row(
+                                children: [
+                                  Radio(
+                                    value: key,
+                                    groupValue: selectedOption3,
+                                    onChanged: (val) {
+                                      setState(() {
+                                        selectedOption3 = key;
+                                      });
+                                    },
+                                    fillColor: MaterialStateProperty
+                                        .resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                        return Colors.black;
+                                      },
+                                    ),
+                                  ),
+                                  Text(
+                                    key,
+                                    style: GoogleFonts.inter(
+                                      color: Colors.black,
+                                      fontSize: Dimensions.screenWidth * 0.045,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }).toList(),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: Dimensions.screenHeight * 0.01,
+                    ),
+                    Container(
+                      width: Dimensions.screenWidth * 0.90,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Pregunta 4',
+                              style: GoogleFonts.inter(
+                                color: const Color.fromRGBO(100, 220, 185, 1),
+                                fontSize: Dimensions.screenWidth * 0.06,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          SizedBox(
+                            height: Dimensions.screenHeight * 0.01,
+                          ),
+                          Text(
+                              '¿Cuál es el capital que se debe invertir para obtener un monto final de 10000 euros al 2% trimestral durante 2 años?',
+                              style: GoogleFonts.inter(
+                                color: Colors.black,
+                                fontSize: Dimensions.screenWidth * 0.045,
+                                fontWeight: FontWeight.w400,
+                              )),
+                          SizedBox(
+                            width: Dimensions.screenHeight * 0.01,
+                          ),
+                          Column(
+                            children: listOptions4!.map((String key) {
+                              return Row(
+                                children: [
+                                  Radio(
+                                    value: key,
+                                    groupValue: selectedOption4,
+                                    onChanged: (val) {
+                                      setState(() {
+                                        selectedOption4 = key;
+                                      });
+                                    },
+                                    fillColor: MaterialStateProperty
+                                        .resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                        return Colors.black;
+                                      },
+                                    ),
+                                  ),
+                                  Text(
+                                    key,
+                                    style: GoogleFonts.inter(
+                                      color: Colors.black,
+                                      fontSize: Dimensions.screenWidth * 0.045,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }).toList(),
+                          )
+                        ],
+                      ),
+                    ),
+                  ]),
                 ),
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: clear,
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(51, 190, 91, 1),
+                      backgroundColor: const Color.fromRGBO(217, 217, 217, 1),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+                        borderRadius: BorderRadius.circular(25),
                       ),
                       minimumSize: Size(
-                          Dimensions.width30, Dimensions.screenHeight * 0.075),
+                          Dimensions.width40, Dimensions.screenHeight * 0.07),
+                    ),
+                    child: Text("Borrar",
+                        style: GoogleFonts.inter(
+                          color: const Color.fromARGB(255, 56, 55, 55),
+                          fontSize: Dimensions.screenWidth * 0.05,
+                          letterSpacing: 1,
+                        )),
+                  ),
+                  ElevatedButton(
+                    onPressed: validation,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(11, 138, 47, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      minimumSize: Size(
+                          Dimensions.width40, Dimensions.screenHeight * 0.07),
                     ),
                     child: Text("Enviar",
                         style: GoogleFonts.inter(
                           color: Colors.white,
-                          fontSize: 17,
+                          fontSize: Dimensions.screenWidth * 0.05,
                           letterSpacing: 1,
                         )),
                   ),
                 ],
-              ),
+              )
             ],
           ),
         ),
       ),
     );
+  }
+
+  clear() {
+    setState(() {
+      selectedOption1 = '';
+      selectedOption2 = '';
+      selectedOption3 = '';
+      selectedOption4 = '';
+    });
+  }
+
+  validation() {
+    int allAnswersCorrect = 0;
+
+    if (selectedOption1 == '300 euros') {
+      allAnswersCorrect++;
+    }
+    if (selectedOption2 == '400 euros') {
+      allAnswersCorrect++;
+    }
+
+    if (selectedOption3 == '1060 euros') {
+      allAnswersCorrect++;
+    }
+    if (selectedOption4 == '9611.57') {
+      allAnswersCorrect++;
+    }
+    if (allAnswersCorrect == 4) {
+      messageResponse("Todas las respuestas son correctas", 'NIVEL EXPERTO');
+    }
+    if (allAnswersCorrect == 3) {
+      messageResponse("Algunas respuestas son correctas", 'NIVEL MEDIO');
+    }
+    if (allAnswersCorrect <= 2) {
+      messageResponse("La mayoria de respuestas son incorrectas", 'NIVEL BAJO');
+    }
+  }
+
+  void messageResponse(String name, String text) {
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: Text(text),
+              content: Text(name),
+            ));
   }
 }
