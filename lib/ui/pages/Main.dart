cdc83704b92d00bc8f780f6faca0fa11ec7ca870
anapatino <asofiapatino@unicesar.edu.co>
2023-11-02 13:36:23 -0500

    change renderflex

--- lib/ui/pages/Main.dart
+++ lib/ui/pages/Main.dart
@@ -15,68 +15,70 @@ class _MainState extends State<Main> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(100, 220, 185, 1),
-      body: Padding(
-        padding: EdgeInsets.symmetric(vertical: Dimensions.height2),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: Dimensions.screenWidth * 0.05),
-              child: Container(
-                  height: Dimensions.screenHeight * 0.32,
-                  width: Dimensions.width90,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/pictures.png'),
-                          fit: BoxFit.contain))),
-            ),
-            Padding(
-              padding:
-                  EdgeInsets.only(left: Dimensions.screenWidth * 0.1, top: 0),
-              child: Text(
-                'Domina conceptos financieros con FinanLearn',
-                style: GoogleFonts.inter(
-                    color: Colors.white,
-                    fontSize: Dimensions.screenHeight * 0.065,
-                    fontWeight: FontWeight.w700),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: Dimensions.height2),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: Dimensions.screenWidth * 0.05),
+                child: Container(
+                    height: Dimensions.screenHeight * 0.32,
+                    width: Dimensions.width90,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/pictures.png'),
+                            fit: BoxFit.contain))),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: Dimensions.screenHeight * 0.03,
-                  bottom: Dimensions.screenHeight * 0.03,
-                  left: Dimensions.screenWidth * 0.1),
-              child: ElevatedButton(
-                onPressed: () {
-                  Get.offAllNamed('/login');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(51, 190, 91, 1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  minimumSize:
-                      Size(Dimensions.width80, Dimensions.buttonHeight),
-                ),
-                child: Text("Ingresar",
-                    style: GoogleFonts.inter(
+              Padding(
+                padding:
+                    EdgeInsets.only(left: Dimensions.screenWidth * 0.1, top: 0),
+                child: Text(
+                  'Domina conceptos financieros con FinanLearn',
+                  style: GoogleFonts.inter(
                       color: Colors.white,
-                      fontSize: Dimensions.screenWidth * 0.05,
-                      letterSpacing: 1.6,
-                    )),
+                      fontSize: Dimensions.screenHeight * 0.065,
+                      fontWeight: FontWeight.w700),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: Dimensions.screenWidth * 0.05),
-              child: Container(
-                  height: Dimensions.screenHeight * 0.10,
-                  width: Dimensions.width90,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/pictures01.png'),
-                          fit: BoxFit.contain))),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(
+                    top: Dimensions.screenHeight * 0.03,
+                    bottom: Dimensions.screenHeight * 0.03,
+                    left: Dimensions.screenWidth * 0.1),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.offAllNamed('/login');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(51, 190, 91, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    minimumSize:
+                        Size(Dimensions.width80, Dimensions.buttonHeight),
+                  ),
+                  child: Text("Ingresar",
+                      style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: Dimensions.screenWidth * 0.05,
+                        letterSpacing: 1.6,
+                      )),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: Dimensions.screenWidth * 0.05),
+                child: Container(
+                    height: Dimensions.screenHeight * 0.10,
+                    width: Dimensions.width90,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/pictures01.png'),
+                            fit: BoxFit.contain))),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/pages/dashboard/dashboard.dart
+++ lib/ui/pages/dashboard/dashboard.dart
@@ -31,149 +31,152 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: Dimensions.height20,
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: -70,
-                    left: -90,
-                    child: Container(
-                      width: 280,
-                      height: 280,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/logo.png'),
-                          fit: BoxFit.fill,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: Dimensions.height20,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: -70,
+                      left: -90,
+                      child: Container(
+                        width: 280,
+                        height: 280,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/logo.png'),
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    top: Dimensions.height2,
-                    left: Dimensions.screenWidth * 0.8,
-                    child: IconButton(
-                        onPressed: () {
-                          Get.offAllNamed('/principal');
-                        },
-                        icon: const Icon(
-                          Icons.login_outlined,
-                          color: Colors.black,
-                          size: 35,
-                        )),
-                  ),
-                  Positioned(
-                    top: Dimensions.screenHeight * 0.095,
-                    left: Dimensions.screenWidth * 0.3,
-                    child: Text("Hola,",
-                        style: GoogleFonts.inter(
+                    Positioned(
+                      top: Dimensions.height2,
+                      left: Dimensions.screenWidth * 0.8,
+                      child: IconButton(
+                          onPressed: () {
+                            Get.offAllNamed('/principal');
+                          },
+                          icon: const Icon(
+                            Icons.login_outlined,
                             color: Colors.black,
-                            fontSize: Dimensions.screenWidth * 0.08,
-                            fontWeight: FontWeight.normal)),
-                  ),
-                  Positioned(
-                    top: Dimensions.screenHeight * 0.135,
-                    left: Dimensions.screenWidth * 0.3,
-                    child: Text(
-                        userController.lastName.isNotEmpty
-                            ? userController.lastName
-                            : 'Usuario',
-                        style: GoogleFonts.inter(
-                            color: Colors.black,
-                            fontSize: Dimensions.screenWidth * 0.1,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                ],
+                            size: 35,
+                          )),
+                    ),
+                    Positioned(
+                      top: Dimensions.screenHeight * 0.095,
+                      left: Dimensions.screenWidth * 0.3,
+                      child: Text("Hola,",
+                          style: GoogleFonts.inter(
+                              color: Colors.black,
+                              fontSize: Dimensions.screenWidth * 0.08,
+                              fontWeight: FontWeight.normal)),
+                    ),
+                    Positioned(
+                      top: Dimensions.screenHeight * 0.135,
+                      left: Dimensions.screenWidth * 0.3,
+                      child: Text(
+                          userController.lastName.isNotEmpty
+                              ? userController.lastName
+                              : 'Usuario',
+                          style: GoogleFonts.inter(
+                              color: Colors.black,
+                              fontSize: Dimensions.screenWidth * 0.1,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: Dimensions.screenWidth * 0.08,
-                top: Dimensions.screenHeight * 0.05,
-                right: Dimensions.screenWidth * 0.08,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Button(
-                        color: const Color.fromRGBO(51, 190, 91, 1),
-                        onPressed: () {
-                          Get.offAll(() => const SimpleInterest());
-                        },
-                        icon: Icons.calculate_rounded,
-                        title: 'Interes simple',
-                      ),
-                      SizedBox(
-                        width: Dimensions.screenWidth * 0.02,
-                      ),
-                      Button(
-                        color: const Color.fromRGBO(255, 227, 179, 1),
-                        onPressed: () {
-                          Get.offAll(() => const CompoundInterest());
-                        },
-                        icon: Icons.calculate_rounded,
-                        title: 'Interes compuesto',
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        top: Dimensions.screenHeight * 0.02,
-                        bottom: Dimensions.screenHeight * 0.03),
-                    child: Row(
+              Padding(
+                padding: EdgeInsets.only(
+                  left: Dimensions.screenWidth * 0.08,
+                  top: Dimensions.screenHeight * 0.05,
+                  right: Dimensions.screenWidth * 0.08,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Button(
-                          color: const Color.fromRGBO(100, 220, 185, 1),
-                          onPressed: () async {
-                            await historyController.viewPublicity();
-                            Get.offAll(() => const History());
+                          color: const Color.fromRGBO(51, 190, 91, 1),
+                          onPressed: () {
+                            Get.offAll(() => const SimpleInterest());
                           },
-                          icon: Icons.format_align_left_rounded,
-                          title: 'Historial',
+                          icon: Icons.calculate_rounded,
+                          title: 'Interes simple',
                         ),
                         SizedBox(
                           width: Dimensions.screenWidth * 0.02,
                         ),
                         Button(
-                          color: const Color.fromRGBO(11, 138, 47, 1),
+                          color: const Color.fromRGBO(255, 227, 179, 1),
                           onPressed: () {
-                            Get.offAll(() => const Test());
+                            Get.offAll(() => const CompoundInterest());
                           },
-                          icon: Icons.play_arrow_rounded,
-                          title: 'Test',
+                          icon: Icons.calculate_rounded,
+                          title: 'Interes compuesto',
                         ),
                       ],
                     ),
-                  ),
-                  SizedBox(
-                    height: Dimensions.screenHeight * 0.06,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: Dimensions.screenWidth * 0.002,
-                          child: Text("Material Educativo",
-                              style: GoogleFonts.inter(
-                                color: Colors.black,
-                                fontSize: Dimensions.screenWidth * 0.075,
-                                fontWeight: FontWeight.bold,
-                              )),
-                        )
-                      ],
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: Dimensions.screenHeight * 0.02,
+                          bottom: Dimensions.screenHeight * 0.03),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Button(
+                            color: const Color.fromRGBO(100, 220, 185, 1),
+                            onPressed: () async {
+                              await historyController.viewPublicity();
+                              Get.offAll(() => const History());
+                            },
+                            icon: Icons.format_align_left_rounded,
+                            title: 'Historial',
+                          ),
+                          SizedBox(
+                            width: Dimensions.screenWidth * 0.02,
+                          ),
+                          Button(
+                            color: const Color.fromRGBO(11, 138, 47, 1),
+                            onPressed: () {
+                              Get.offAll(() => const Test());
+                            },
+                            icon: Icons.play_arrow_rounded,
+                            title: 'Test',
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: Dimensions.screenHeight * 0.06,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: Dimensions.screenWidth * 0.002,
+                            child: Text("Material Educativo",
+                                style: GoogleFonts.inter(
+                                  color: Colors.black,
+                                  fontSize: Dimensions.screenWidth * 0.075,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-                height: Dimensions.screenHeight * 0.3, child: viewPublicity()),
-          ],
+              SizedBox(
+                  height: Dimensions.screenHeight * 0.3,
+                  child: viewPublicity()),
+            ],
+          ),
         ),
       ),
     );
